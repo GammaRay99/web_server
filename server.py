f72cd15ed9235227d656ae13912bb63576d80329
GammaRay99 <52253862+GammaRay99@users.noreply.github.com>
2021-03-21 18:56:13 +0100

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,17 @@ app.init()
 
 @app.load_path("/home")
 def home():
-	return """<h1>Hello World</h1>"""
+	app.response_type = "text/html"
+	return f"<h1>{app.read_file('./dynamic/count.wb')} post requests</h1>"
+
+@app.load_path("/home", method="POST")
+def home():
+	app.response_type = "json"
+	count = int(app.read_file('./dynamic/count.wb')) + 1
+	with open('./dynamic/count.wb', "w") as file:
+		file.write(str(count))
+
+	return "{'current count': " + str(count) + "}"
 
 
-app.run(keep_log=False)
+app.run(keep_log=True)
