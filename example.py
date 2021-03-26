88746730108e79d3d35db5823280a23729c78c19
GammaRay99 <52253862+GammaRay99@users.noreply.github.com>
2021-03-26 23:30:19 +0100

    Update example.py

--- example.py
+++ example.py
@@ -4,11 +4,14 @@ import webserver
 app = webserver.WebApp(port=666)
 app.init()
 
-
 @app.load_path("/home")
 def home():
+	count = int(app.read_file('./dynamic/count.wb')) + 1
+	with open('./dynamic/count.wb', "w") as file:
+		file.write(str(count))
+
 	app.response_type = "text/html"
-	return f"<h1>{app.read_file('./dynamic/count.wb')} post requests</h1>"
+	return f"<h1>{str(count)} post requests</h1>"
 
 @app.load_path("/home", method="POST")
 def home():
