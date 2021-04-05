11d6ddd424b764d4a3de0451d8c36afe347d3bcc
GammaRay99 <52253862+GammaRay99@users.noreply.github.com>
2021-04-05 17:38:40 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,26 +1,61 @@
 import webserver
 
 
-app = webserver.WebApp(port=666)
+app = webserver.WebApp(port=8080)
 app.init()
 
-@app.load_path("/home")
+counters = { 'carrot': 10 }
+etoile = lambda: len(counters)
+
+
+@app.load_path(["", "star"])
 def home():
-	count = int(app.read_file('./dynamic/count.wb')) + 1
-	with open('./dynamic/count.wb', "w") as file:
-		file.write(str(count))
+	app.response.headers["Content-type"] = "text/html"
+	return f"{etoile()} counters"
+
+
+@app.load_path([""], method="POST")
+def post_counter():
+	if app.request.body != "":
+		app.response.headers["Content-type"] = "json"
+		counters[app.request.body] = 0
+		return "{" + app.request.body + ": 0}"
+	else:
+		app.response.headers["Content-type"] = "text/plain"
+		return "Le nouveau compteur doit avoir un nom"
+
+
+@app.load_path(counters.keys())
+def get_counter():
+	path = app.request.path
+	counter_name = path[1:]
+
+	app.response.headers["Content-type"] = "text/html"
+	return f"{counters[counter_name]} {counter_name}"
+
+
+@app.load_path(counters.keys(), method="PUT")
+def add_counter():
+	name = app.request.path[1:]
+
+	app.response.headers["Content-type"] = "text/plain"
+
+	if name in counters.keys():
+		counters[name] = app.request.body if app.request.body != "" else counters[name] + 1
+		return "Done"
+	else:
+		return "Ce compteur n'existe pas"
+
+
+@app.load_path(counters.keys(), method="DELETE")
+def del_counter():
+	path = app.request.path
+
+	if path[1:] in counters.keys():
+		counters.pop(path[1:])
 
 	app.response_type = "text/html"
-	return f"<h1>{str(count)} post requests</h1>"
-
-@app.load_path("/home", method="POST")
-def home():
-	app.response_type = "json"
-	count = int(app.read_file('./dynamic/count.wb')) + 1
-	with open('./dynamic/count.wb', "w") as file:
-		file.write(str(count))
-
-	return "{'current count': " + str(count) + "}"
+	return ""
 
 
 app.run(keep_log=True)
